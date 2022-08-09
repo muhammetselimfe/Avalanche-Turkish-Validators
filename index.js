3b492ac0c0258aa4332b97a54e5346427075781e
Muhammet Selim Ferah <46397353+muhammetselimfe@users.noreply.github.com>
2022-08-09 15:32:01 +0200

    change API source

--- index.js
+++ index.js
@@ -5,126 +5,134 @@ function yuvarla(sayi, basamak) {
   return Math.round(sayi * basamak) / basamak
 }
 
-axios.get('https://vscout.io/dev/api/validators')
-  .then((response) => {
-    const validators = response.data.validators
-    //console.log(validators)        
-    //console.log(search("NodeID-EmPwabyobnM3jYDvQuxZdLBTut5V5pq2n", validators))
+axios.post('http://95.70.184.178:9950/ext/bc/P', {
+  "jsonrpc": "2.0",
+  "method": "platform.getCurrentValidators",
+  "params": {},
+  "id": 1
+}, {
+  headers: {
+    'content-type': 'application/json'
+  }
+}).then((response) => {
+  const validators = response.data.result.validators
+  console.log(validators)        
+  //console.log(search("NodeID-EmPwabyobnM3jYDvQuxZdLBTut5V5pq2n", validators))
 
-    axios.get('https://raw.githubusercontent.com/muhammetselimfe/validator-list/master/validators.json')
-      .then((response) => {
+  axios.get('https://raw.githubusercontent.com/muhammetselimfe/validator-list/master/validators.json')
+    .then((response) => {
 
-        const validatorList = response.data
+      const validatorList = response.data
 
-        //console.log(validatorList[0].username)       
+      //console.log(validatorList[0].username)       
 
-        var tablo = document.createElement("table")
-        tablo.setAttribute("id", "t01")
-        var trtr = document.createElement("tr")
-        tablo.appendChild(trtr)
+      var tablo = document.createElement("table")
+      tablo.setAttribute("id", "t01")
+      var trtr = document.createElement("tr")
+      tablo.appendChild(trtr)
 
-        var username = document.createElement("th")
-        trtr.appendChild(username)
-        username.innerText = "Kullanıcı Adı"
+      var username = document.createElement("th")
+      trtr.appendChild(username)
+      username.innerText = "Kullanıcı Adı"
 
-        var Nodeid = document.createElement("th")
-        trtr.appendChild(Nodeid)
-        Nodeid.innerText = "Validator Node ID"
+      var Nodeid = document.createElement("th")
+      trtr.appendChild(Nodeid)
+      Nodeid.innerText = "Validator Node ID"
 
-        let fee = document.createElement("th")
-        trtr.appendChild(fee)
-        fee.innerText = "Fee(%)"
+      let fee = document.createElement("th")
+      trtr.appendChild(fee)
+      fee.innerText = "Fee(%)"
 
-        let capacity = document.createElement("th")
-        trtr.appendChild(capacity)
-        capacity.innerText = "Kapasite"
+      // let capacity = document.createElement("th") *-*-*-*-*-*-
+      // trtr.appendChild(capacity)
+      // capacity.innerText = "Kapasite"
 
-        let location = document.createElement("th")
-        trtr.appendChild(location)
-        location.innerText = "Lokasyon"
+      // let location = document.createElement("th") *-*-*-*-
+      // trtr.appendChild(location)
+      // location.innerText = "Lokasyon"
 
-        let upTime = document.createElement("th")
-        trtr.appendChild(upTime)
-        upTime.innerText = "UPTIME"
+      let upTime = document.createElement("th")
+      trtr.appendChild(upTime)
+      upTime.innerText = "UPTIME"
 
-        validatorList.forEach(element => {
+      validatorList.forEach(element => {
 
-          const test = search(element.nodeID, validators)
-          //console.log(test)
-          test.forEach(element2 => {
-            var fee = yuvarla(element2.delegationFee, 2)
-            var kap = element2.remainingCapacity
-            //console.log(kap)
+        const test = search(element.nodeID, validators)
+        //console.log(test)
+        test.forEach(element2 => {
+          var fee = yuvarla(element2.delegationFee, 2)
+          var kap = element2.remainingCapacity
+          //console.log(kap)
 
-            var tr = document.createElement("tr")
-            var usernameRow = document.createElement("td")
-            //usernameRow.setAttribute("id","user")
-            usernameRow.style.cssText = "height: 60px;"
-            var nodeIDRow = document.createElement("td")
-            var feeRow = document.createElement("td")
-            var CapRow = document.createElement("td")
-            var LocationRow = document.createElement("td")
-            var upTimeRow = document.createElement("td")
+          var tr = document.createElement("tr")
+          var usernameRow = document.createElement("td")
+          //usernameRow.setAttribute("id","user")
+          usernameRow.style.cssText = "height: 60px;"
+          var nodeIDRow = document.createElement("td")
+          var feeRow = document.createElement("td")
+          // var CapRow = document.createElement("td") *-*-*-*-
+          // var LocationRow = document.createElement("td") *-*-*-
+          var upTimeRow = document.createElement("td")
 
-            //tr.innerHTML = element.username
-            usernameRow.innerHTML = `<a href="https://t.me/${element.username}" target="_blank">${element.username}</a>` //element.username
-            nodeIDRow.innerHTML = `<a href="https://vscout.io/validator/${element2.nodeID}" target="_blank">${element2.nodeID}</a>` //element2.nodeID
-            feeRow.innerHTML = + fee
-            CapRow.innerHTML = parseInt(element2.remainingCapacity / 10 ** 9)
-            LocationRow.innerHTML = element2.country
-            upTimeRow.innerHTML = parseInt(element2.uptime * 100)
+          //tr.innerHTML = element.username
+          usernameRow.innerHTML = `<a href="https://t.me/${element.username}" target="_blank">${element.username}</a>` //element.username
+          nodeIDRow.innerHTML = `<a href="https://vscout.io/validator/${element2.nodeID}" target="_blank">${element2.nodeID}</a>` //element2.nodeID
+          feeRow.innerHTML = + fee
+          // CapRow.innerHTML = parseInt(element2.remainingCapacity / 10 ** 9) *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+          // LocationRow.innerHTML = element2.country *-*-*-*-*-*-*-
+          upTimeRow.innerHTML = parseInt(element2.uptime * 100)
 
-            tr.appendChild(usernameRow)
-            tr.appendChild(nodeIDRow)
-            tr.appendChild(feeRow)
-            tr.appendChild(CapRow)
-            tr.appendChild(LocationRow)
-            tr.appendChild(upTimeRow)
+          tr.appendChild(usernameRow)
+          tr.appendChild(nodeIDRow)
+          tr.appendChild(feeRow)
+          // tr.appendChild(CapRow) *-*-*-*-*-*---
+          // tr.appendChild(LocationRow) *-*-*-*-*-*-
+          tr.appendChild(upTimeRow)
 
-            tablo.appendChild(tr)
+          tablo.appendChild(tr)
 
-            document.body.appendChild(tablo)
+          document.body.appendChild(tablo)
 
-          })
-        });
+        })
+      });
 
-        // order operations
-        const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+      // order operations
+      const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-        const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
-          v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-        )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+      const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+      )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
-        // do the work...
-        document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-          const table = th.closest('table');
-          Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-            .forEach(tr => table.appendChild(tr));
-        })));
-        // Code Source: https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript/49041392#49041392
+      // do the work...
+      document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+        const table = th.closest('table');
+        Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+          .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+          .forEach(tr => table.appendChild(tr));
+      })));
+      // Code Source: https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript/49041392#49041392
 
-        var footer = document.createElement("footer")
-        footer.setAttribute("id", "footer")
-        footer.innerHTML = "Made with ❤️"
-        document.body.appendChild(footer)
+      var footer = document.createElement("footer")
+      footer.setAttribute("id", "footer")
+      footer.innerHTML = "Made with ❤️"
+      document.body.appendChild(footer)
 
-      })
-  }).catch((error) => {
-    if (error.response) {
-      //The response status is an error code
-      console.log(error.response.status);
-      var h1 = document.getElementById("title")
-      h1.innerText = "Service is temporarily unavailable"
+    })
+}).catch((error) => {
+  if (error.response) {
+    //The response status is an error code
+    console.log(error.response.status);
+    var h1 = document.getElementById("title")
+    h1.innerText = "Service is temporarily unavailable"
 
-    } else if (error.request) {
-      //Response not received though the request was sent
-      console.log(error.request);
-    } else {
-      //An error occurred when setting up the request
-      console.log(error.message);
-    }
-  });
+  } else if (error.request) {
+    //Response not received though the request was sent
+    console.log(error.request);
+  } else {
+    //An error occurred when setting up the request
+    console.log(error.message);
+  }
+});
 
 /**** */
 const price = document.getElementById('price')
